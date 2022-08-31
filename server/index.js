b9bb7f4f23a74905f96db3d930a9665b10fe1762
Zamzam93 <samira0101@live.com>
2022-08-31 15:38:10 -0500

    bug fix

--- server/index.js
+++ server/index.js
@@ -4,11 +4,11 @@ const path = require('path');
 const app = express();
 
 app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, '../public.index.js'));
+    res.sendFile(path.join(__dirname, '../index.html'));
 });
 
 const port = process.env.PORT|| 4005;
-//this will get the port variable from Heroku. however, if one isnt assigned it will use port 4005.
+
 
 app.listen(port, () => {
     console.log(`Listening on the port ${port}`);
