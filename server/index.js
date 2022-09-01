1c7f37ac2e6980b0d138ff24928cbe201ebc7800
Zamzam93 <samira0101@live.com>
2022-09-01 14:50:28 -0500

    edited

--- server/index.js
+++ server/index.js
@@ -1,7 +1,30 @@
 const express = require('express');
 const path = require('path');
-
+const cors  = require('cors')
 const app = express();
+app.use(cors())
+
+// include and initialize the rollbar library with your access token
+var Rollbar = require('rollbar')
+var rollbar = new Rollbar({
+  accessToken: '87694effeeb34d0fb360bc2eb9fdb370',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
+
+// record a generic message and send it to Rollbar
+rollbar.log('Hello world!')
+
+try {
+    nonExistentFunction();
+  } catch (error) {
+    console.error(error);
+    // expected output: ReferenceError: nonExistentFunction is not defined
+    // Note - error messages will vary depending on browser
+  }
+
+
+
 
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, '../index.html'));
